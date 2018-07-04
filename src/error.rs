1364cda7649a7fe180fc969d712ff724b760367f
Cameron Ruatta <cameron@ruatta.com>
2018-07-04 12:09:39 -0700

    Adding some more robust error handling

---
+++ src/error.rs
@@ -0,0 +1,75 @@
+//error.rs
+
+use std::fmt;
+use std::error::Error;
+
+use ffi::*;
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub enum InternalErrorKind {
+    End,
+    Internal,
+    Load,
+    Depack,
+    Invalid,
+    State,
+    System,
+    Other,
+    BadFormat
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub enum SelfErrorKind {
+    Other
+}
+
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub enum ErrorKind {
+    SelfType(SelfErrorKind),
+    InternalType(InternalErrorKind)
+}
+
+
+pub fn from_int_error_code(ret: i32) -> ErrorKind {
+    let kind = match ret.abs() {
+        XMP_END => ErrorKind::InternalType(InternalErrorKind::End),
+        XMP_ERROR_INTERNAL => ErrorKind::InternalType(InternalErrorKind::Internal),
+        XMP_ERROR_FORMAT => ErrorKind::InternalType(InternalErrorKind::BadFormat),
+        XMP_ERROR_LOAD => ErrorKind::InternalType(InternalErrorKind::Load),
+        XMP_ERROR_DEPACK => ErrorKind::InternalType(InternalErrorKind::Depack),
+        XMP_ERROR_SYSTEM => ErrorKind::InternalType(InternalErrorKind::System),
+        XMP_ERROR_INVALID => ErrorKind::InternalType(InternalErrorKind::Invalid),
+        XMP_ERROR_STATE => ErrorKind::InternalType(InternalErrorKind::State),
+        _ => ErrorKind::InternalType(InternalErrorKind::Other)
+    };
+    kind
+}
+
+#[derive(Debug)]
+pub struct XmpError {
+    pub details: String,
+    pub kind: ErrorKind
+}
+
+impl XmpError {
+    pub fn new(msg: &str, kind: ErrorKind) -> XmpError {
+        XmpError{ details: msg.to_string(), kind }
+    }
+}
+
+impl fmt::Display for XmpError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
+impl Error for XmpError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@
 extern crate libc;
 
 use std::path::Path;
-use std::ffi::{ CString, CStr };
-use std::fmt;
-use std::error::Error;
+use std::ffi::{  CString, CStr };
 
+mod error;
 mod ffi;
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+    use super::error::*;
 
     #[test]
     fn test_test_module_xm() {
@@ -31,102 +31,52 @@ mod tests {
         let path = Path::new("./test/");
 
         if let Err(x) = test_module(&path) {
-            assert_eq!(x.kind, ErrorKind::System)
+            assert_eq!(x.kind, ErrorKind::InternalType(InternalErrorKind::System))
         }
     }
 
     #[test]
-    fn test_test_module_bad_path() {
+    fn test_test_module_missing_path() {
         let path = Path::new("./test/bad");
 
         if let Err(x) = test_module(&path) {
-            assert_eq!(x.kind, ErrorKind::System)
+            assert_eq!(x.kind, ErrorKind::InternalType(InternalErrorKind::System))
         }
     }
 }
 
-#[derive(Debug)]
-pub struct TestModuleError {
-    details: String,
-    kind: ErrorKind
-}
-
-impl TestModuleError {
-    fn new(msg: &str, kind: ErrorKind) -> TestModuleError {
-        TestModuleError{details: msg.to_string(), kind }
-    }
-}
-
-impl fmt::Display for TestModuleError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"{}",self.details)
-    }
-}
-
-impl Error for TestModuleError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
 
 pub struct TestModuleInfo {
     t_name: String,
     t_type: String
 }
 
-#[derive(PartialEq)]
-#[derive(Debug)]
-enum ErrorKind {
-    Internal,
-    Load,
-    Depack,
-    Invalid,
-    State,
-    System,
-    Other,
-    BadFormat
-}
 
-pub fn test_module(path: &Path) -> Result<TestModuleInfo, TestModuleError> {
+pub fn test_module(path: &Path) -> Result<TestModuleInfo, error::XmpError> {
     use ffi::*;
 
-    if path.is_dir() {
-        return Err(TestModuleError::new("path is dir", ErrorKind::System))
+    let p = path.to_string_lossy();
+
+    let (test_info, ret) = unsafe {
+        let mut test_info: xmp_test_info = std::mem::uninitialized();
+        let ret = xmp_test_module(CString::new(p.as_ref()).unwrap().as_ptr(), &mut test_info);
+            (test_info, ret)
     };
 
+    if ret != 0 {
+        let int_kind = error::from_int_error_code(ret);
+        return Err(error::XmpError::new(&format!("xmp_test_module call failed with code: {}", ret), int_kind));
+    };
 
-    if let Some(p) = path.to_str() {
+    let (t_name, t_type) = unsafe {
+        let t_name = CStr::from_ptr(test_info.t_name.as_ptr());
+        let t_name = t_name.to_owned().into_string().unwrap();
+        let t_type = CStr::from_ptr(test_info.t_type.as_ptr());
+        let t_type = t_type.to_owned().into_string().unwrap();
 
-        let test_info: xmp_test_info = unsafe {
-            let mut module: xmp_test_info = std::mem::uninitialized();
-            let ret = xmp_test_module(CString::new(p).unwrap().as_ptr(), &mut module);
-            if ret != 0 {
-                let kind = match ret.abs() {
-                     XMP_END => ErrorKind::Other,
-                     XMP_ERROR_INTERNAL => ErrorKind::Internal,
-                     XMP_ERROR_FORMAT => ErrorKind::BadFormat,
-                     XMP_ERROR_LOAD => ErrorKind::Load,
-                     XMP_ERROR_DEPACK => ErrorKind::Depack,
-                     XMP_ERROR_SYSTEM => ErrorKind::System,
-                     XMP_ERROR_INVALID => ErrorKind::Invalid,
-                     XMP_ERROR_STATE => ErrorKind::State,
-                    _ => ErrorKind::Other
-                };
-                return Err(TestModuleError::new(&format!("xmp_test_module call failed with code: {}", ret), kind))
-            };
-            module
-        };
+        (t_name, t_type)
+    };
 
-        let test_info: TestModuleInfo = unsafe {
-            let t_name = CStr::from_ptr(test_info.t_name.as_ptr());
-            let t_name = t_name.to_owned().into_string().unwrap();
-            let t_type = CStr::from_ptr(test_info.t_type.as_ptr());
-            let t_type = t_type.to_owned().into_string().unwrap();
-            TestModuleInfo{ t_name, t_type }
-        };
-        return Ok(test_info);
-    }
-    return Err(TestModuleError::new("path doesn't exist", ErrorKind::System));
-
+    return Ok(TestModuleInfo{ t_name, t_type });
 }
 
